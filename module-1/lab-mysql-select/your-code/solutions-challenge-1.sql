64418a832a6e53048a95c5b1cd1badf4192efe39
Julie GRANGE <grange.julie@gmail.com>
2019-07-03 18:42:41 +0200

    ajout des solutions complètes

---
+++ module-1/lab-mysql-select/your-code/solutions-challenge-1.sql
@@ -0,0 +1,5 @@
+SELECT a.au_id, a.au_lname, a.au_fname, t.title, p.pub_name
+from publications.titleauthor ta
+join publications.authors a on ta.au_id = a.au_id
+join publications.titles t on ta.title_id = t.title_id
+join publications.publishers p on t.pub_id = p.pub_id;
---
+++ module-1/lab-mysql-select/your-code/solutionsV2.sql
@@ -0,0 +1,33 @@
+
+-- Challenge 1
+SELECT a.au_id, a.au_lname, a.au_fname, t.title, p.pub_name
+from publications.titleauthor ta
+join publications.authors a on ta.au_id = a.au_id
+join publications.titles t on ta.title_id = t.title_id
+join publications.publishers p on t.pub_id = p.pub_id;
+
+-- Challenge 2
+SELECT a.au_id, a.au_lname, a.au_fname, p.pub_name, COUNT(*) AS Titres
+from publications.titleauthor ta
+join publications.authors a on ta.au_id = a.au_id
+join publications.titles t on ta.title_id = t.title_id
+join publications.publishers p on t.pub_id = p.pub_id
+GROUP bY a.au_id, a.au_lname, a.au_fname, p.pub_name;
+
+
+-- Challenge 3
+SELECT a.au_id, a.au_lname, a.au_fname, COUNT(ta.title_id) AS Titres
+from publications.titleauthor ta
+right join publications.authors a on ta.au_id = a.au_id
+GROUP bY a.au_id, a.au_lname, a.au_fname
+ORDER BY Titres desc, au_id LIMIT 3;
+
+-- Challenge 4
+select
+a.au_lname, a.au_fname, count(ta.title_id) as Titre
+from authors a
+left join titleauthor ta on ta.au_id = a.au_id
+group by a.au_id
+order by Titre desc
+;
+
